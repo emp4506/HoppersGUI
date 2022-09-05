f31a19bfbb7611359e455066731dc5bf6be9c3f1
Evan Prizel <emp4506@rit.edu>
2022-09-04 23:03:50 -0400

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module BFSPuzzleSolver {
+    requires transitive javafx.controls;
+    exports puzzles.common;
+    exports puzzles.common.solver;
+    exports puzzles.hoppers.gui;
+    exports puzzles.hoppers.ptui.model;
+    exports puzzles.jam.gui;
+    exports puzzles.jam.model;
+}
